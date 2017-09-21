b8f4f9ecc6419012f90b03e32122b3e614292410
Joshua <JoshHLangley@gmail.com>
2017-09-21 15:00:01 +0100

    Fixed some bugs.

--- app/src/main/java/com/github/heavyhat/blobcounter/MainActivity.kt
+++ app/src/main/java/com/github/heavyhat/blobcounter/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.github.heavyhat.blobcounter
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.hardware.Camera
 import android.os.Bundle
+import android.util.Log
+import android.widget.FrameLayout
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    fun getCameraInstance() : Camera?
+    {
+        var c : Camera?;
+        c = null;
+        try
+        {
+            c = Camera.open(0);
+        }
+        catch (e : Exception)
+        {
+            Log.e("ERROR", "Can't open camera");
+        }
+        return c;
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var camera = getCameraInstance();
+        if(camera != null) {
+            var preview = CameraPreview(this, camera);
+            var layout = findViewById(R.id.camera_preview) as FrameLayout;
+            layout.addView(preview);
+            camera.release();
+        }
+        else
+        {
+            Toast.makeText(this, "Couldn't get Camera.", Toast.LENGTH_LONG).show()
+        }
     }
 }
