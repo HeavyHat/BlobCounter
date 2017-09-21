9d4a4a53dbe48efd460f4923bb866d49ab99cc1e
Joshua <JoshHLangley@gmail.com>
2017-09-21 12:35:54 +0100

    Converted the main activity to Kotlin

---
+++ app/src/test/java/com/github/heavyhat/blobcounter/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package com.github.heavyhat.blobcounter
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+class ExampleUnitTest
+{
+
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+
+}
